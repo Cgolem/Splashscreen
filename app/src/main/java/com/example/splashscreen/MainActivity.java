9362dbadf991ce6833c04bfdd098be8b37e989b7
Carlos <carlos_td20@comunidad.unam.mx>
2019-01-27 12:59:03 -0600

    Documentación de código

--- app/src/main/java/com/example/splashscreen/MainActivity.java
+++ app/src/main/java/com/example/splashscreen/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.splashscreen;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Generamos un hilo
+        Thread threadSplash = new Thread() {
+
+            //Sobreescribimos el método run()
+            @Override
+            public void run() {
+                try {
+                    //Retardo de 3s
+                    sleep(3000);
+
+                    //Creamos un objeto Intent, este nos permitirá poder llamar a otra actividad.
+                    //Enviaremos el contexto de la app y la clase a la que iremos, es decir, a la cual cambiaremos de
+                    //pantalla. pantalla 1 ---> pantalla 2
+                    Intent intentSplash = new Intent(getApplicationContext(), Home.class);
+
+                    //Iniciamos nuestro Intent
+                    startActivity(intentSplash);
+
+                    //Finalizamos lka actividad en la cual estamos actualmente, es decir "MainActivity"
+                    finish();
+                } catch (Exception err) {
+                    //capturamos la excepcion que pudiera suceder e imprimimos en consola
+                    err.printStackTrace();
+                }
+            }
+        };
+        threadSplash.start();
     }
 }
